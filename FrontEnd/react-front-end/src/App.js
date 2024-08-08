0a34f863827ff6f27f080805ff8fe072590a707b
Vijay Singh <vijaysingh3225@gmail.com>
2024-08-07 23:12:17 -0400

    First full stack data traversal success

--- FrontEnd/react-front-end/src/App.js
+++ FrontEnd/react-front-end/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [tradeExecs, setTradeExecs] = useState([]);
+
+  const getTradeExecs = async () => {
+    try {
+      const response = await api.get('/api/tradeExecs');
+      console.log('Received response:', response.data);
+      setTradeExecs(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getTradeExecs();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        MarketStack
       </header>
+      <ul>
+        {tradeExecs.map((tradeExec, index) => (
+          <li key={index}>
+            {/* Customize how you display each tradeExec */}
+            {JSON.stringify(tradeExec)}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
