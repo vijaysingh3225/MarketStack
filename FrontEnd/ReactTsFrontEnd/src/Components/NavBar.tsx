548276ac16c2bb6351a1ab4c0309fbfa8495851f
Vijay Singh <vijaysingh3225@gmail.com>
2024-09-18 00:48:59 -0400

    navigation bar near completion

--- FrontEnd/ReactTsFrontEnd/src/Components/NavBar.tsx
+++ FrontEnd/ReactTsFrontEnd/src/Components/NavBar.tsx
@@ -5,11 +5,32 @@ const NavBar: React.FC = () => {
   return <div className='navbar'>
     <div className='title'></div>
     <div className='navContainer'>
-        <div className='navItem'>Dashboard</div>
-        <div className='navItem'></div>
-        <div className='navItem'></div>
-        <div className='navItem'></div>
-        <div className='navItem'></div>
+        <div className='navItem'>
+          <img src="src\images\dashboard-white.png" alt="" className='logo'/>
+          <span>Dashboard</span></div>
+        <div className='navItem'>
+          <img src="src\images\graphLogo-white.png" alt="" className='logo'/>
+          <span>Graphs</span>
+        </div>
+        <div className='navItem'>
+          <span>Statistics</span>
+        </div>
+        <div className='navItem'>
+          <span>Calendar</span>
+        </div>
+        <div className='navItem'>
+          <span>Visualizer</span>
+        </div>
+    </div>
+    <div className='navFooter'>
+      <div className='footerItem'>
+        <img src="src\images\importIcon.png" alt="" className='logo'/>
+        <span>Import</span>
+        </div>
+      <div className='footerItem spacer'>
+      <img src="src\images\accountIcon.png" alt="" className='logo'/>
+        <span>Account</span>
+        </div>
     </div>
   </div>;
 };
