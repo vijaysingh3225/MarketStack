66688006486ac16d3649064a8cccb95f92124614
Vijay Singh <vijaysingh3225@gmail.com>
2024-09-25 23:27:04 -0400

    deploy testing

--- FrontEnd/ReactTsFrontEnd/vite.config.ts
+++ FrontEnd/ReactTsFrontEnd/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this is set to 'dist'
+  },
   server: {
     proxy: {
       '/api': {
