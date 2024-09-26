3b5d5bb41237dc65a32439f182f29ea538edf83a
Vijay Singh <vijaysingh3225@gmail.com>
2024-09-25 23:36:56 -0400

    deploy testing

--- FrontEnd/ReactTsFrontEnd/src/Components/HomeNav.tsx
+++ FrontEnd/ReactTsFrontEnd/src/Components/HomeNav.tsx
@@ -6,13 +6,13 @@ const HomeNav: React.FC = () => {
   return (
     <div className='nav'>
         <div className='title'>
-        <img src="src/images/TitleLogo.png" alt="" className='titleLogo' />
+        <img src="public/images/TitleLogo.png" alt="" className='titleLogo' />
       </div>
       <div className='nav-buttons'>
         <NavLink to="/" className='nav-item'><div>Home</div></NavLink>
         <NavLink to="/Login" className='nav-item'><div>Features</div></NavLink>
         <NavLink to="/Login" className='nav-item'><div>Sign In</div></NavLink>
-        <NavLink to="/Login" className='nav-item'><div>Register</div></NavLink>
+        <NavLink to="/Dashboard" className='nav-item'><div>Register</div></NavLink>
         
       </div>
     </div>
