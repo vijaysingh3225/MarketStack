5693df190867d9594f8e14174465ef3e88495518
Vijay Singh <vijaysingh3225@gmail.com>
2024-09-27 09:10:25 -0400

    routine push

--- BackEnd/build.gradle.kts
+++ BackEnd/build.gradle.kts
@@ -1,40 +1,41 @@
 plugins {
-	alias(libs.plugins.spring.boot)
-	id("io.spring.dependency-management") version "1.1.6"
-	kotlin("jvm") version libs.versions.kotlinVersion
-	kotlin("plugin.spring") version libs.versions.kotlinVersion
+    alias(libs.plugins.spring.boot)
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version libs.versions.kotlinVersion
+    kotlin("plugin.spring") version libs.versions.kotlinVersion
 }
 
 group = "quest.marketstack"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("io.mockk:mockk:1.13.5")
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.mockk:mockk:1.13.5")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
