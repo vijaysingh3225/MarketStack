02c39b6dacc971214f38763482a6d6de9d23335d
Vijay Singh <vijaysingh3225@gmail.com>
2024-09-29 22:53:15 -0400

    CORZ testing

--- FrontEnd/ReactTsFrontEnd/src/Components/PnlHistogram.tsx
+++ FrontEnd/ReactTsFrontEnd/src/Components/PnlHistogram.tsx
@@ -36,7 +36,7 @@ const PnlHistogram: React.FC<PnlHistogramProps> = ({ tradeCount }) => {
   useEffect(() => {
    
     axios
-      .get("http://44.204.163.10:8080/api/v1/closedTrades")
+      .get("https://44.204.163.10:8080/api/v1/closedTrades")
       .then((response) => {
         const sortedTrades = response.data.sort(
           (a: Trade, b: Trade) =>
