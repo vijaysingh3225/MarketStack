a3962e4d38edf960ba9227af4557c679ab51f7e8
Vijay Singh <vijaysingh3225@gmail.com>
2024-10-07 00:00:24 -0400

    more ssl configuration not working:

--- FrontEnd/ReactTsFrontEnd/src/Components/TradeList.tsx
+++ FrontEnd/ReactTsFrontEnd/src/Components/TradeList.tsx
@@ -23,7 +23,7 @@ const TradeList: React.FC = () => {
 
   useEffect(() => {
     axios
-      .get("https://api.marketstack.quest/api/v1/closedTrades")
+      .get("https://backend.marketstack.quest/api/v1/closedTrades")
       .then((response) => {
       
         const sortedTrades = response.data.sort(
