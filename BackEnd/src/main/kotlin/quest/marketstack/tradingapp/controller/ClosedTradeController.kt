127ad6331b343bc9ec1de87098b4889d54ca803b
Vijay Singh <vijaysingh3225@gmail.com>
2024-10-08 00:46:09 -0400

    Full Stack Connection Complete!

--- BackEnd/src/main/kotlin/quest/marketstack/tradingapp/controller/ClosedTradeController.kt
+++ BackEnd/src/main/kotlin/quest/marketstack/tradingapp/controller/ClosedTradeController.kt
@@ -7,7 +7,7 @@ import quest.marketstack.tradingapp.model.ClosedTrade
 import quest.marketstack.tradingapp.service.closedtrades.ClosedTradeServiceInterface
 
 @RestController
-@CrossOrigin(origins = ["https://backend.marketstack.quest"])
+@CrossOrigin(origins = ["https://marketstack.quest"])
 @RequestMapping("\${api.closed-trade.base-url}")
 class ClosedTradeController(
     private val service: ClosedTradeServiceInterface,
